afc05f26237a66a00edf519a478a2fe2bfd1f179
BBaoVanC <bbaovanc@bbaovanc.com>
2022-03-22 16:30:53 -0500

    Fix repo_init.sh by removing -t from git checkout

--- repo_init.sh
+++ repo_init.sh
@@ -4,7 +4,7 @@ cd
 git init
 git remote add origin https://github.com/BBaoVanC/dotfiles.git
 git fetch
-git checkout -tf origin/master
+git checkout -f origin/master
 git am < downloads/0001-Remove-ssh-override-on-gitconfig.patch
 git submodule update --init .config/ranger/plugins/ranger_devicons
 git -C .config/ranger/plugins/ranger_devicons checkout main
